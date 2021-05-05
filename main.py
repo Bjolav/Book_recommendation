2f0544b2d9a1a06a25f00868b9acb51f5d8f5797
Bjørn André <46919344+Bjolav@users.noreply.github.com>
2021-05-05 23:03:44 +0200

    Kivy + SparQL
    Added Kivy and SpaQL support. Program crashes if any button is interacted with. csv_load somehow started giving errors all of a sudden when used in triples()

--- main.py
+++ main.py
@@ -1,6 +1,65 @@
+import pandas as pd
+import random
 from rdflib import Graph, Literal, Namespace, URIRef
 from rdflib.namespace import RDF, RDFS, OWL, XSD
 import pandas as pd
+from rdflib.namespace import RDF, RDFS, OWL
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.uix.recycleview import RecycleView
+from kivy.lang import Builder
+from kivy.properties import ObjectProperty
+from kivy.uix.popup import Popup
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+kv = Builder.load_file("my.kv")
+
+
+class Menu(FloatLayout):
+    btn = ObjectProperty(None)
+    book = ObjectProperty(None)
+    author = ObjectProperty(None)
+
+    def btn(self):
+        show_popup(Randomized())
+
+    def btn2(self):
+        show_popup(BookSearch)
+
+    def btn3(self):
+        show_popup(AuthorSearch)
+
+
+class Randomized(FloatLayout):
+    def __init__(self, **kwargs):
+        super(Randomized, self).__init__(**kwargs)
+        self.data = [{"text": str(random_book(csv_loader()))}]
+
+
+class BookSearch(RecycleView, Menu):
+    def __init(self, **kwargs):
+        super(BookSearch, self).__init__(**kwargs)
+        self.data = [{"text": str(x) for x in range(book_search(csv_loader(), Menu.book))}]
+
+
+class AuthorSearch(RecycleView, Menu):
+    def __init(self, **kwargs):
+        super(AuthorSearch, self).__init__(**kwargs)
+        self.data = [{"text": str(x) for x in range(author_search(csv_loader(), Menu.author))}]
+
+
+class MyApp(App):
+    def build(self):
+        return Menu()
+
+
+def show_popup(instance):
+    popupWindow = Popup(title="Result", content=instance)
+    popupWindow.open()
 
 
 def csv_loader():
@@ -8,7 +67,6 @@ def csv_loader():
     csv_data = pd.read_csv("books.csv", error_bad_lines=False, encoding="utf-8")
 
     # Deletion of all rows we deem unnecessary
-    del csv_data['bookID']
     del csv_data['isbn']
     del csv_data['ratings_count']
     del csv_data['text_reviews_count']
@@ -41,9 +99,7 @@ def csv_loader():
     return csv_data
 
 
-
-def main():
-    csv_loader()
+def triples(csv_data):
     g = Graph()
     g.parse("https://www.wikidata.org/wiki/Q571", format="xml")
     schema = Namespace("https://schema.org/")
@@ -52,9 +108,6 @@ def main():
     g.bind("rdf", RDF)
     g.bind("owl", OWL)
     g.bind("xsd", XSD)
-
-
-    csv_data = pd.read_csv("books.csv", error_bad_lines=False, encoding="utf-8")
 
     csv_data = csv_data.fillna("unknown")
 
@@ -65,7 +118,6 @@ def main():
 
         subject = row['title']
         g.add((URIRef(schema + subject), RDF.type, OWL.DatatypeProperty))
-
         g.add((URIRef(schema + subject), URIRef(schema + "identifier"), Literal(row["isbn13"], datatype=XSD.int)))
         g.add((URIRef(schema + subject), URIRef(schema + "author"), URIRef(schema + row["authors"], datatype=XSD.string)))
         g.add((URIRef(schema + subject), URIRef(schema + "aggregateRating"), Literal(row["average_rating"], datatype=XSD.float)))
@@ -80,7 +132,132 @@ def main():
             g.add((URIRef(schema + subject), OWL.oneOf, Literal(row["series"])))
             g.add((URIRef(schema + subject), URIRef(schema + "partOfSeries"), URIRef(schema + row["series"], datatype=XSD.string)))
 
-    g.remove((None, None, URIRef("hhtps://schema.org/unknown")))
+    g.remove((None, None, URIRef("https://schema.org/unknown")))
 
-    #print(g.serialize(format="turtle").decode("utf-8"))
-    g.serialize(destination='output.owl', format='turtle')
+    # print(g.serialize(format="turtle").decode("utf-8"))
+    return g.serialize(destination='output.owl', format='turtle')
+    # print(g.serialize(format="turtle").decode())
+
+
+def random_book(data):
+    num = random.randint(1, (len(data) + 1))
+    return data.iloc[num]
+
+
+def author_search(data, author):
+    search = author.lower()
+    search = search.replace(' ', '_')
+
+    results = {}
+
+    for ind in data.index:
+        if search in data['authors'][ind]:
+            val = data['bookID'][ind]
+            results[val] = []
+            results[val].append(data['bookID'][ind])
+            results[val].append(data['title'][ind])
+            results[val].append(data['authors'][ind])
+            results[val].append(data['average_rating'][ind])
+            results[val].append(data['language_code'][ind])
+            results[val].append(data['num_pages'][ind])
+            results[val].append(data['publication_date'][ind])
+            results[val].append(data['publisher'][ind])
+            results[val].append(data['series'][ind])
+            results[val].append(data['book_number'][ind])
+
+    return results
+
+
+def book_search(data, author):
+    search = author
+    search = search.replace(' ', '_')
+
+    results = {}
+
+    for ind in data.index:
+        if search in data['title'][ind]:
+            val = data['bookID'][ind]
+            results[val] = []
+            results[val].append(data['bookID'][ind])
+            results[val].append(data['title'][ind])
+            results[val].append(data['authors'][ind])
+            results[val].append(data['average_rating'][ind])
+            results[val].append(data['language_code'][ind])
+            results[val].append(data['num_pages'][ind])
+            results[val].append(data['publication_date'][ind])
+            results[val].append(data['publisher'][ind])
+            results[val].append(data['series'][ind])
+            results[val].append(data['book_number'][ind])
+
+    return results
+
+
+def sparql():
+    sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
+
+    # Spørring som henter alle bøkene som en forfatter (her: J.K. Rowling) har skrevet
+    sparql.setQuery("""
+    select ?book ?bookLabel where
+    { ?book wdt:P50 wd:Q34660.
+      SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+    }
+
+    """)
+
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+
+    for result in results["results"]["bindings"]:
+        print(result["book"]["value"], "   ", result["bookLabel"]["value"])
+
+    # Spørring som henter sjangrene til en bok eller en serie av bøker (her: Harry Potter), siden datasettet vårt ikke inkluderer sjangre
+    sparql.setQuery("""
+    select ?genre ?genreLabel where
+    { wd:Q43361 wdt:P136 ?genre.
+       SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+    }
+
+    """)
+
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+
+    for result in results["results"]["bindings"]:
+        print(result["genre"]["value"], "   ", result["genreLabel"]["value"])
+
+    # Spørring som finner 10 bøker som har samme sjanger som en bok eller serie (her: Harry Potter)
+    sparql.setQuery("""
+    select ?book ?bookLabel where
+    { wd:Q43361 wdt:P136 ?genre.
+     ?book wdt:P136 ?genre.
+     SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+     }
+     limit 10""")
+
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+
+    for result in results["results"]["bindings"]:
+        print(result["book"]["value"], "   ", result["bookLabel"]["value"])
+
+    # Spørring som henter alle bøker av en bestemt forfatter, som har samme sjanger som en bestemt forfatter (her: Harry Potter og J.K. Rowling)
+    sparql.setQuery("""
+    select distinct ?book ?bookLabel where
+    { wd:Q43361 wdt:P136 ?genre.
+     ?book wdt:P136 ?genre.
+     ?book wdt:P50 wd:Q34660.
+     SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+    }
+    """)
+
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+
+    for result in results["results"]["bindings"]:
+        print(result["book"]["value"], "   ", result["bookLabel"]["value"])
+
+
+if __name__ == '__main__':
+    #triples(csv_loader())
+    sparql()
+    MyApp().run()
